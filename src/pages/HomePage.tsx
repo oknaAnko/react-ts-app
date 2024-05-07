394fc6592b4801e89f142670f85f0b8bd59a092e
annajedrzejko <anna.jedrzejko@elpassion.pl>
2024-05-07 16:13:52 +0200

    Fetch all recipes

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,8 +1,49 @@
-import React from 'react'
+import axios from 'axios'
+import React, { useEffect, useState } from 'react'
+import { useNavigate } from 'react-router-dom'
+import Button from '../shared/Button'
+import { Recipe } from '../types'
+
+// const defaultRecipes: Recipe[] = [
+//   {
+// id: "",
+//     title: 'Cupcakes',
+//     time: '20 min',
+//     difficulty: 'easy',
+//     category: 'sweets',
+//     description: 'lorem ipsum...'
+//   }
+// ]
 
 const HomePage = () => {
+  const navigate = useNavigate();
+  const [recipes, setRecipes] = useState<Recipe[]>([])
+  // console.log('recipes', recipes);
+
+  const getRecipes = () => {
+    axios.get('http://localhost:3033/recipes')
+      .then(res => res.data)
+      .then(data => setRecipes(data))
+  }
+
+  useEffect(() => {
+    getRecipes()
+  }, [])
+
   return (
-    <div>HomePage</div>
+    <>
+      <div>HomePage</div>
+      {recipes.map(recipe => (
+        <>
+          <Button element='link' variant='primary' onClick={() => navigate(`/recipes/${recipe._id}`)} >
+            {recipe.title}
+          </Button>
+          <p>{recipe.time}</p>
+          <p>{recipe.difficulty}</p>
+          <p>{recipe.category}</p>
+        </>
+      ))}
+    </>
   )
 }
 
--- src/types.ts
+++ src/types.ts
@@ -1,4 +1,5 @@
 export interface Recipe {
+  _id?: string,
   title: string,
   time: string,
   difficulty: string,
