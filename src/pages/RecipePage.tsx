843aebcd1814f2601401d91b9ef7967f6e112a3a
annajedrzejko <anna.jedrzejko@elpassion.pl>
2024-05-07 16:14:04 +0200

    Fetch single recipe

--- src/pages/RecipePage.tsx
+++ src/pages/RecipePage.tsx
@@ -1,8 +1,35 @@
-import React from 'react'
+import axios from 'axios';
+import React, { useEffect, useState } from 'react'
+import { useParams } from 'react-router-dom';
+import { Recipe } from '../types';
 
 const RecipePage = () => {
+  const { recipeId } = useParams();
+  const [recipe, setRecipe] = useState<Recipe | null>(null)
+
+  const getRecipe = () => {
+    axios.get(`http://localhost:3033/recipes/${recipeId}`)
+      .then(res => res.data)
+      .then(data => setRecipe(data))
+  }
+
+  useEffect(() => {
+    getRecipe()
+  }, [])
+
+
   return (
-    <div>RecipePage</div>
+    <>
+      <div>RecipePage</div>
+      {recipe &&
+        <>
+          <p>{recipe.title}</p>
+          <p>{recipe.time}</p>
+          <p>{recipe.difficulty}</p>
+          <p>{recipe.category}</p>
+        </>
+      }
+    </>
   )
 }
 
